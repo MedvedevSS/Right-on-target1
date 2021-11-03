4da60c8165b4b0de70607eeea04ae845d0463b7b
devGod <devgod@mail.ru>
2021-11-03 11:41:36 +0300

    Initial Commit

---
+++ Right on target1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on target1
+//
+//  Created by Sergey Medvedev on 03.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
