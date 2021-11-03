6a66c894169277485c952d0445d9dbf2e30ae4fe
devGod <devgod@mail.ru>
2021-11-03 11:50:49 +0300

    1. Создали дизайн приложения 2. Сделали бизнес - логику

--- Right on target1/ViewController.swift
+++ Right on target1/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var label: UILabel!
+    
+    var number = 0
+    var round = 0
+    var points = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func checkNumber() {
+        if round == 0 {
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+            round = 1
+        } else {
+            let numSlider = Int(slider.value.rounded())
+            if numSlider > number {
+                points += 50 - numSlider - number
+            } else if numSlider < number {
+                points += 50 - number + numSlider
+            } else {
+                points += 50
+            }
+            if round == 5 {
+                let alert = UIAlertController(title: "Игра окончена", message: "Вы набрали \(points) очков", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+                round = 1
+                points = 0
+            } else {
+                round += 1
+            }
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+        }
+    }
 
 }
 
